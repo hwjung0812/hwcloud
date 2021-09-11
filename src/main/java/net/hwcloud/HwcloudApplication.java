af7220206c4c76099083ffad5d2c660226d89cf3
DESKTOP-S38CAND\muhan <muhanrush@outlook.com>
2021-09-11 10:07:58 +0900

    war build 설정 내용 추가

--- src/main/java/net/hwcloud/HwcloudApplication.java
+++ src/main/java/net/hwcloud/HwcloudApplication.java
@@ -2,11 +2,18 @@ package net.hwcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class HwcloudApplication {
+public class HwcloudApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HwcloudApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HwcloudApplication.class, args);
